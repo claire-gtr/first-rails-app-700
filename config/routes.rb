31e928085a0e03438e43b0b8f460f9346d655770
claire gautier <claire.gautier@edhec.com>
2021-08-16 10:25:05 +0200

    finished lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #VERB '/path', to: 'controller#action'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  root to: 'pages#homepage'
 end
